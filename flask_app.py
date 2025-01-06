030691c340b9024c43cb937ada15a5d0789ce3cb
gunishmatta2 <gunish.matta@golocad.com>
2025-01-07 02:48:05 +0530

    fix cors

--- flask_app.py
+++ flask_app.py
@@ -1,10 +1,14 @@
 import logging
 
 from flask import Flask, jsonify, request
+from flask_cors import CORS
 
 from app.ai.pipeline import RagPipeSingleton
 
 app = Flask(__name__)
+
+CORS(app, resources={r"/chat": {"origins": "34.142.215.180"}})
+
 
 ERROR_MISSING_QUESTION = "Question is required."
 ERROR_INTERNAL_SERVER = "An internal error occurred."
